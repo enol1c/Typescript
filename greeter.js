a560f6d81f585f6fa18e863ca18ffc6f96566f8f
Nicole Strebel <hi@enolic.ch>
2017-02-05 17:14:03 +0100

    Change Name
    To Susan

--- greeter.js
+++ greeter.js
@@ -1,7 +1,7 @@
 function greeter(person) {
     return "Hi " + person.firstname + " " + person.lastname;
 }
-var user = { firstname: 'Nicole', lastname: "Strebel" };
+var user = { firstname: 'Susan', lastname: "Strebel" };
 window.onload = function () {
     var h1 = document.querySelector('h1');
     h1.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -7,7 +7,7 @@ function greeter(person: Person){
   return "Hi " + person.firstname + " " + person.lastname;
 }
 
-var user = {firstname: 'Nicole', lastname: "Strebel"};
+var user = {firstname: 'Susan', lastname: "Strebel"};
 window.onload = () => {
    var h1: HTMLElement = document.querySelector('h1');
    h1.innerHTML = greeter(user);
